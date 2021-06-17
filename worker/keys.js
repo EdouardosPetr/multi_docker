7b3f33e723e9435371fb8d2555c8bd8904dc4ac1
Edouardos Petriadis <epetriadis@gmail.com>
2021-06-17 23:22:40 +0200

    initial commit

---
+++ worker/keys.js
@@ -0,0 +1,10 @@
+/* 
+
+Anytime the worker sees a new index added going to pull that index and then calculate the appropriate Fibonacci value for it
+
+*/
+
+module.exports = {
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT,
+};
